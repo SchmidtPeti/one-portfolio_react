796fa758fa40c8234710c2e4d130c2bef3f295a2
Schmidt Péter <schmidt.peti32@gmail.com>
2022-10-23 19:55:24 +0200

    portfolio website

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    About
+                </p>
+            </div>
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sapiente temporibus aut eos nisi quod aliquid. Magnam dolorum vel nesciunt? Debitis quod perferendis non laboriosam, dolorem iusto delectus laudantium assumenda natus eius architecto aspernatur obcaecati in pariatur. Architecto, nostrum, maiores ducimus sint libero autem hic tempore eos, quibusdam numquam in.
+            </p>
+            <br />
+            <p className='text-xl'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iste quisquam sed ad suscipit error esse officiis architecto debitis ipsum culpa impedit voluptatum blanditiis dolorum corrupti iusto aliquam tempora dignissimos a facere magnam rerum voluptatibus, nisi veritatis! Inventore quidem, repellendus quisquam veritatis commodi ipsa asperiores ea ut omnis, sequi cumque?
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
